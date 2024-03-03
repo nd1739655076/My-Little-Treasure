d5ed76d503cfb2bc72c88f887453321561596cb9
TszyaoEkaterina <88958534+TszyaoEkaterina@users.noreply.github.com>
2024-03-03 00:30:18 -0500

    signup.js changed to using localstorage

--- signup.js
+++ signup.js
@@ -6,76 +6,29 @@ function createUser() {
     password = document.getElementById("password").value;
     console.log("name:"+name+", password:"+password);
     const outputElement = document.getElementById("err text");
-    // Create a new XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-    var fileContent = "";
-    // Define a callback function to handle the response
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) { // Check if the request is complete
-            if (xhr.status === 200) { // Check if the request was successful
-                // If successful, display the content of the file
-                //outputElement.textContent = xhr.responseText;
-                fileContent = xhr.responseText;
-                console.log(fileContent);
-                var lines = fileContent.split('\n');
-                console.log(lines[0]);
-                lines.forEach(function(line) {
-                    // Check if the line is defined and not empty
-                    if (line && line.trim() !== '') {
-                        // Split the line into words based on a space character
-                        var words = line.split(' ');
-                        
-                        // Get the first word (index 0) from the array of words
-                        var firstWord = words[0];
-                        console.log("name:"+name+",first word:"+firstWord);
-                        
-                        // Compare the first word to the search term
-                        if (firstWord === name) {
-                            // If the first word matches the search term, do something
-                            console.log("Match found: " + line);
-                            outputElement.textContent = "This user already exist!";
-                        }
-                    }
-                });
-                appendToFile(name, password);
-                window.location.href = "index.html";
-            } else {
-                // If there was an error, display an error message
-                outputElement.textContent = "Error loading user.txt";
-            }
-        }
-    };
+    outputElement.textContent = "";
+    var user = localStorage.getItem(name);
+    if (user === null) {
+        // name does not exist in localStorage
+        console.log('The key does not exist in localStorage.');
+        writeToFile(name, password);
+    } else {
+        // Key exists, handle the value
+        console.log("not empty");
+        console.log("Match found: " + user[0] + user[1]);
+        setTimeout(function() {
+            // This code will be executed after 1 second
+            console.log("1 second has passed");}, 1000);
+        outputElement.textContent = "This user already exist!";
+    }
 
-    // Open the file using a GET request
-    xhr.open("GET", "./users.txt", true);
-
-    // Send the request
-    xhr.send();
-    //TODO: write into file
 }
 
-function appendToFile(name, password) {
-    var xhr = new XMLHttpRequest();
-    // Define the data to be sent
-    var data = `${encodeURIComponent(name)} ${encodeURIComponent(password)}`;
-
-    // Define a callback function to handle the response
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) { // Check if the request is complete
-            if (xhr.status === 200) { // Check if the request was successful
-                // If successful, display a success message (optional)
-                console.log("User information appended successfully.");
-            } else {
-                // If there was an error, display an error message (optional)
-                console.error("Error appending user information:", xhr.statusText);
-            }
-        }
-    };
-
-    // Open the file using a POST request to append data
-    xhr.open("POST", "./users.txt", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    // Send the request with the user information
-    xhr.send(data);
+function writeToFile(name, password) {
+    let data = [];
+    data.push(name);
+    data.push(password);
+    console.log("data array:"+data[0]+data[1]);
+    // Data to write to the file.
+    localStorage.setItem(name, JSON.stringify(data));
 }
