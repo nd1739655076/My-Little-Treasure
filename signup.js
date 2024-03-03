9b9969a53c1ff39456757c0067417667841590a0
TszyaoEkaterina <3022805283@qq.com>
2024-03-03 10:30:05 -0500

    all set

--- signup.js
+++ signup.js
@@ -1,37 +1,36 @@
-<!DOCTYPE html>
-<html lang = "en">
-<head>
-    <meta charset = "UTF-8">
-    <meta http-equiv="X-UA-Compatible" content = "IE = edge">
-    <meta name = "viewport" , initial-scale="1.0">
-    <title>LASTEST | Codehal</title>
-    <link rel = "stylesheet" href = "signup.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' 
-    rel='stylesheet'>
-</head>
-<body>
-    <div class = "wrapper">
-        <form action = "">
-            <h1> Sign Up </h1>
-            <div class = "input-box">
-                <input id = "username" type = "text" placeholder = "Username" required>
-                <i class='bx bxs-invader'></i>
-            </div>
-            <div class = "input-box">
-                <input id = "password" type = "password"
-                placeholder = "Password" required>
-                <i class='bx bxs-lock-alt'></i>
-            </div>
-            <button type = "button" onclick="createUser()" class = "btn">Submit</button>
-            <div class = "register-link">
-                <p> Alredy have an account? <a
-                href = "index.html"> Click Me</a></p>
-            </div>
-            <div>
-                <p id = "err text"></p >
-            </div>  
-        </from>
-    </div>
-    <script src="signup.js"></script>
-</body>
-</html>
+let name;
+let password;
+function createUser() {
+    name = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    console.log("name:"+name+", password:"+password);
+    const outputElement = document.getElementById("err text");
+    outputElement.textContent = "";
+    var user = localStorage.getItem(name);
+    if (user === null) {
+        // name does not exist in localStorage
+        console.log('The key does not exist in localStorage.');
+        writeToFile(name, password);
+        window.location.href = "./dashboard.html?username=" + encodeURIComponent(name);
+    } else {
+        // Key exists, handle the value
+        console.log("not empty");
+        console.log("Match found: " + user[0] + user[1]);
+        setTimeout(function() {
+            // This code will be executed after 1 second
+            console.log("1 second has passed");}, 1000);
+        outputElement.textContent = "This user already exist!";
+    }
+}
+function writeToFile(name, password) {
+    let data = [];
+    data.push(name);
+    data.push(password);
+    data.push(0);
+    data.push(0);
+    data.push(0);
+    data.push([]);
+    console.log("data array:"+data[0]+data[1]);
+    // Data to write to the file.
+    localStorage.setItem(name, JSON.stringify(data));
+}
